6b9cbd383afc20e171ea782a86380755b38f51b4
Hakai <moinkhanrobin1@gmail.com>
2023-12-27 18:11:05 +0530

    Time-Stamp Microservice Test 1

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const currentTime = new Date()
+  res.json({greeting: currentTime});
 });
 
 
