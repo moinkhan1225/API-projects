6b6300933155e6a3487b0304966eb4d0a87e048e
Hakai <moinkhanrobin1@gmail.com>
2023-12-27 21:16:42 +0530

    Time-Stamp Microservice Test 40

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.get("/api/:date", function (req, res) {
   //date = new Date();
   const currentTime = new Date();
   //const intTime=parseInt(currentTime);
-  if(date=="date") return res.json(
+  if(date==currentTime) return res.json(
     {
-      unix: currentTime.getMilliseconds(),
+      unix: currentTime.getTime(),
       utc:  currentTime.toUTCString()
       
     });else if(date!="date") return res.json({
